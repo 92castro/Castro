bd6e9971b58d605cb03f33df46b52808e6043820
92castro <celin92castro@gmail.com>
2023-09-21 21:03:39 -0600

    update about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,13 +8,12 @@ export default function About() {
       <section className="about">
         <Grid>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            I am Celin Castro, a proficient full-stack web developer with an
+            extensive background in sales. My ability to fluently speak both
+            Spanish and English has greatly facilitated effective communication
+            and collaboration with diverse individuals. What distinguishes me is
+            my unwavering resilience, adaptability, and influential abilities,
+            which contribute to my success in the field.
           </Typography>
         </Grid>
       </section>
