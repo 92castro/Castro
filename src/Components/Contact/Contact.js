d0b68ee4105514ef4464f9531cc956576e69c411
92castro <celin92castro@gmail.com>
2023-09-25 17:57:42 -0600

    update contact section

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,12 +1,35 @@
 import "../Contact/Contact.css";
 import React from "react";
-import { Typography } from "@mui/material";
+import { Typography, Grid, Button } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
 
 export default function Contact() {
   return (
     <div className="main">
       <section className="contact">
-        <Typography>CONTACT</Typography>
+        <Typography>CONNECT WITH ME</Typography>
+        <Grid>
+          <Button
+            style={{ transform: "scale(2)" }}
+            startIcon={<LinkedInIcon />}
+            href="https://www.linkedin.com/in/celin-castro/"
+            target="_blank"
+          />
+          <Button
+            style={{ transform: "scale(2)" }}
+            startIcon={<GitHubIcon />}
+            href="https://github.com/92castro"
+            target="_blank"
+          />
+          <Button
+            style={{ transform: "scale(2)" }}
+            startIcon={<EmailIcon />}
+            href="mailto:celin92castro@gmail.com"
+            target="_blank"
+          />
+        </Grid>
       </section>
     </div>
   );
