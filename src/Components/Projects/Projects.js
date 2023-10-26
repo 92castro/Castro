9ca8cc09c6f0ae8c1f706aeb8f265f5d815ba95a
92castro <celin92castro@gmail.com>
2023-10-25 21:43:45 -0600

    fix sizing of carousel photos and added links to sites

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,6 +2,7 @@ import "../Projects/Projects.css";
 import React from "react";
 import { Typography, Box, Grid } from "@mui/material";
 import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 //  images
 import goldengoose from "../../Assets/goldengoosef.PNG";
@@ -11,41 +12,55 @@ import overland from "../../Assets/overland.PNG";
 // import usbank from "../../assets/sponsors/usbank.png";
 // import zynex from "../../assets/sponsors/zynex.jpg";
 
-const responsive = {
-  0: { items: 1 },
-  568: { items: 2 },
-  1024: { items: 3 },
-};
+// const responsive = {
+//   0: { items: 1 },
+//   568: { items: 2 },
+//   1024: { items: 3 },
+// };
 
 const items = [
-  <img
-    src={goldengoose}
-    alt="Golden Goose"
-    className="item"
-    data-value="1"
-    style={
-      {
-        // width: "300px",
-        // display: "flex",
-        // margin: "auto",
-        // paddingTop: "50px",
-      }
-    }
-  />,
-  <img
-    src={fermentations}
-    alt="Fermentations"
-    className="item"
-    data-value="2"
-    // style={{ width: "300px", display: "flex", margin: "auto" }}
-  />,
-  <img
-    src={overland}
-    alt="Overland Baseball"
-    className="item"
-    data-value="3"
-    // style={{ width: "300px", display: "flex", margin: "auto" }}
-  />,
+  <a href="https://golden-goose-finance.herokuapp.com/">
+    <img
+      src={goldengoose}
+      alt="Golden Goose"
+      className="item"
+      data-value="1"
+      style={{
+        width: "700px",
+        display: "flex",
+        margin: "auto",
+        paddingTop: "50px",
+      }}
+    />
+  </a>,
+  <a href="https://fermentationsdemo.netlify.app/">
+    <img
+      src={fermentations}
+      alt="Fermentations"
+      className="item"
+      data-value="2"
+      style={{
+        width: "700px",
+        display: "flex",
+        margin: "auto",
+        paddingTop: "50px",
+      }}
+    />
+  </a>,
+  <a href="https://overlandbaseball.com/">
+    <img
+      src={overland}
+      alt="Overland Baseball"
+      className="item"
+      data-value="3"
+      style={{
+        width: "700px",
+        display: "flex",
+        margin: "auto",
+        paddingTop: "50px",
+      }}
+    />
+  </a>,
   // <img src={security} alt="security credit union" className="item" data-value="4" style={{ width: "300px", display: "flex", margin: "auto" }} />,
   // <img src={usbank} alt="usbank" className="item" data-value="5" style={{ width: "300px", display: "flex", margin: "auto" }} />,
   // <img src={zynex} alt="zynex" className="item" data-value="6" style={{ width: "300px", display: "flex", margin: "auto" }} />,
@@ -57,16 +72,22 @@ export default function Projects() {
       <Grid className="carousel">
         <Box>
           <AliceCarousel
-            autoPlay
-            disableDotsControls
-            mouseTracking
-            items={items}
-            responsive={responsive}
-            controlsStrategy="alternate"
+            animationType="fadeout"
+            animationDuration={800}
+            disableButtonsControls
             infinite
-            autoPlayStrategy="none"
-            autoPlayInterval={1000}
-            animationDuration={3000}
+            items={items}
+            mouseTracking
+            // autoPlay
+            // disableDotsControls
+            // mouseTracking
+            // items={items}
+            // responsive={responsive}
+            // controlsStrategy="alternate"
+            // infinite
+            // autoPlayStrategy="none"
+            // autoPlayInterval={1000}
+            // animationDuration={3000}
           />
         </Box>
       </Grid>
