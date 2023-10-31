899b131cbaa639450bb9ee43c98c91894f8367aa
92castro <celin92castro@gmail.com>
2023-10-30 21:16:18 -0600

    change about me paragraph

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,21 +1,21 @@
 import "../About/About.css";
 import React from "react";
-import { Grid, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function About() {
   return (
     <div className="main">
       <section className="about">
-        <Grid>
-          <Typography>
-            I am Celin Castro, a proficient full-stack web developer with an
-            extensive background in sales. My ability to fluently speak both
-            Spanish and English has greatly facilitated effective communication
-            and collaboration with diverse individuals. What distinguishes me is
-            my unwavering resilience, adaptability, and influential abilities,
-            which contribute to my success in the field.
+        <Box>
+          <Typography className="aboutMe" fontSize={"25px"}>
+            I'm a web developer and sales professional who values collaboration
+            and positivity. I'm dedicated to creating user-friendly websites and
+            believe in the strength of teamwork. When I'm not coding, I enjoy
+            hiking and playing video games. With a friendly and optimistic
+            outlook, I'm always eager to connect, share ideas, and make the most
+            of every opportunity.
           </Typography>
-        </Grid>
+        </Box>
       </section>
     </div>
   );
