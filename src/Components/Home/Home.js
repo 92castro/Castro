b3cc60d359d002523fe5421fcbf82a48db869fa5
92castro <celin92castro@gmail.com>
2023-10-30 21:04:53 -0600

    add typedjs into home section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,41 @@
 import "../Home/Home.css";
 import React from "react";
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
 import { Box, Typography } from "@mui/material";
 import photo from "../../Assets/me-2.png";
 
 export default function Home() {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "DEVELOPER",
+        "SALES PROFESSIONAL",
+        "INTREPID",
+        "HIKER",
+        "GAMER",
+        "BREWER",
+      ],
+      startDelay: 300,
+      typeSpeed: 150,
+      backDelay: 150,
+      fadeOut: true,
+      showCursor: false,
+      loop: true,
+    });
+  });
   return (
     <div className="main">
       <section className="home">
         <Box>
           <div class="flyinTxtCont">
             <h1 class="flyIn lineOne">Celin Castro</h1>
-            <Typography>An Intrepid Human Being</Typography>
           </div>
+          <Typography>
+            |<span ref={el}></span>
+          </Typography>
         </Box>
         <Box>
           <img className="photo" src={photo} />
