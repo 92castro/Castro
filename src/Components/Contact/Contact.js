d5949e94e0a92f477c832c8e3dfc5d98bc2bb115
92castro <celin92castro@gmail.com>
2023-11-02 17:04:49 -0600

    add contact form to contact section

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,15 +1,54 @@
 import "../Contact/Contact.css";
-import React from "react";
-import { Typography, Grid, Button } from "@mui/material";
+import React, { useState } from "react";
+import { Grid, Button } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
+import emailjs from "emailjs-com";
 
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Replace with your Email.js service ID, template ID, and user ID
+    const serviceID = "your_email_service_id";
+    const templateID = "your_template_id";
+    const userID = "your_user_id";
+
+    emailjs
+      .send(serviceID, templateID, formData, userID)
+      .then((response) => {
+        console.log("Email sent successfully:", response);
+      })
+      .catch((error) => {
+        console.error("Email sending failed:", error);
+      });
+
+    // Clear the form fields after submission
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="main">
       <section className="contact">
-        <Typography>CONNECT WITH ME</Typography>
+        <h1>CONNECT WITH ME</h1>
         <Grid className="socials">
           <Button
             style={{ transform: "scale(3)" }}
@@ -30,6 +69,36 @@ export default function Contact() {
             target="_blank"
           />
         </Grid>
+
+        <div className="contact-form">
+          <h2>Contact Me</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleInputChange}
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+            <textarea
+              name="message"
+              placeholder="Message"
+              value={formData.message}
+              onChange={handleInputChange}
+              required
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
       </section>
     </div>
   );
